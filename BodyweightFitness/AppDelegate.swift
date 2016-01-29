c070ecef146cecc9ee605f76d64d38764c737cb8
Damian Mazurkiewicz <damian@mazur.io>
2016-01-29 16:31:51 +0000

    Ooooops

--- BodyweightFitness/AppDelegate.swift
+++ BodyweightFitness/AppDelegate.swift
@@ -21,7 +21,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let sideViewController = storyboard.instantiateViewControllerWithIdentifier("SideViewController") as! SideViewController
-        g
+        
         mainViewController = storyboard.instantiateViewControllerWithIdentifier("MainViewController")
         calendarViewController = storyboard.instantiateViewControllerWithIdentifier("CalendarNavigationController")
         settingsViewController = storyboard.instantiateViewControllerWithIdentifier("NavigationSettingsController")
