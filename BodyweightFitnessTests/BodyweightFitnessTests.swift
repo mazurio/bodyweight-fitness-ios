2e04d57ca2d9dd9c8dd0977a654a9621677d1ea2
Damian Mazurkiewicz <damian@mazur.io>
2016-04-20 11:06:28 +0100

    Task: Add Unit Tests Module

---
+++ BodyweightFitnessTests/BodyweightFitnessTests.swift
@@ -0,0 +1,24 @@
+import XCTest
+@testable import Bodyweight_Fitness
+
+class BodyweightFitnessTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+
+        let routine = Routine()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testExample() {
+        XCTAssertTrue(true)
+    }
+    
+    func testPerformanceExample() {
+        XCTAssertTrue(false)
+    }
+    
+}
