78292d608d7c7755fa4d1e6582cd2072b3ebbbee
Damian Mazurkiewicz <damian@mazur.io>
2016-08-30 20:41:27 +0100

    Add + icon and switcher between routines

--- BodyweightFitness/Controller/HomeViewController.swift
+++ BodyweightFitness/Controller/HomeViewController.swift
@@ -14,19 +14,65 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
         
         self.setNavigationBar()
-        
-        let menuItem = UIBarButtonItem(
+    
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(
             image: UIImage(named: "menu"),
             landscapeImagePhone: nil,
             style: .Plain,
             target: self,
-            action: #selector(dismiss))
-
-        self.navigationItem.leftBarButtonItem = menuItem
-        self.navigationItem.title = "Home"
+            action: #selector(dismiss)
+        )
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(
+            image: UIImage(named: "plus"),
+            landscapeImagePhone: nil,
+            style: .Plain,
+            target: self,
+            action: #selector(routine)
+        )
+        
+        self.navigationItem.title = "Bodyweight Fitness"
     }
     
     func dismiss(sender: UIBarButtonItem) {
         self.sideNavigationController?.toggleLeftView()
+    }
+    
+    func routine(sender: UIBarButtonItem) {
+        let alertController = UIAlertController(
+            title: "Choose Workout Routine",
+            message: nil,
+            preferredStyle: .ActionSheet)
+        
+        alertController.popoverPresentationController
+        alertController.modalPresentationStyle = .Popover
+        
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        
+        alertController.addAction(UIAlertAction(title: "Bodyweight Fitness", style: .Default) { (action) in
+            print("Test")
+        })
+        
+        alertController.addAction(UIAlertAction(title: "Molding Mobility", style: .Default) { (action) in
+            print("Test")
+        })
+        
+        alertController.addAction(UIAlertAction(title: "Starting Stretching", style: .Default) { (action) in
+            print("Test")
+        })
+        
+        self.presentViewController(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func startWorkout(sender: AnyObject) {
+        let appDelegate = UIApplication.sharedApplication().delegate as? AppDelegate
+        
+        // TODO this does not select menu item on the left
+        sideNavigationController?.transitionFromRootViewController(
+            (appDelegate?.workoutViewController)!,
+            duration: 0,
+            options: UIViewAnimationOptions.CurveEaseIn,
+            animations: nil,
+            completion: nil)
     }
 }
