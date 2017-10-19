679ccfb80d0c0b4ea728dfc2267be17fbca86127
Damian Mazurkiewicz <damian@mazur.io>
2017-10-19 13:24:38 +0100

    T

--- BodyweightFitness/WorkoutLog/WorkoutLogGeneralSnapKitViewController.swift
+++ BodyweightFitness/WorkoutLog/WorkoutLogGeneralSnapKitViewController.swift
@@ -63,77 +63,6 @@ class CardButton: UIButton {
 }
 
 class WorkoutLogGeneralSnapKitViewController: UIViewController {
-    
-    func first() {
-        let card = CardView()
-        let secondCard = CardView()
-        
-        let stackView = UIStackView()
-        self.view.addSubview(stackView)
-        
-        stackView.axis = UILayoutConstraintAxis.vertical
-        stackView.distribution = UIStackViewDistribution.equalSpacing
-        stackView.alignment = UIStackViewAlignment.top
-        stackView.spacing = 16.0
-        
-        stackView.addArrangedSubview(card)
-        stackView.addArrangedSubview(secondCard)
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        
-        stackView.snp.makeConstraints { (make) -> Void in
-            make.width.equalTo(self.view)
-            make.height.equalTo(self.view)
-            make.center.equalTo(self.view)
-        }
-        
-        card.snp.makeConstraints { (make) -> Void in
-            make.width.equalTo(stackView)
-            make.height.equalTo(100)
-        }
-        
-        secondCard.snp.makeConstraints { (make) -> Void in
-            make.width.equalTo(stackView)
-            make.height.equalTo(100)
-        }
-        
-        let label = TitleLabel()
-        label.text = "Title"
-        
-        card.addSubview(label)
-        label.snp.makeConstraints { (make) -> Void in
-            make.top.equalTo(card).offset(20)
-            make.left.equalTo(card).offset(16)
-            make.right.equalTo(card).offset(-16)
-            
-            make.height.equalTo(24)
-        }
-        
-        let textView = DescriptionTextView()
-        textView.text = "Many people want to improve overall flexibility, but do not know where to begin. This routine should serve as a general jumping-off point for beginners."
-        card.addSubview(textView)
-        
-        let cardButton = CardButton()
-        cardButton.setTitle("Read More", for: .normal)
-        card.addSubview(cardButton)
-        
-        textView.snp.makeConstraints { (make) -> Void in
-            make.top.equalTo(label.snp.bottom).offset(8)
-            make.left.equalTo(card).offset(12)
-            make.right.equalTo(card).offset(-12)
-            make.bottom.equalTo(cardButton.snp.top).offset(8)
-            
-            make.height.equalTo(100)
-        }
-        
-        cardButton.snp.makeConstraints { (make) -> Void in
-            make.left.equalTo(card).offset(16)
-            make.right.equalTo(card).offset(-16)
-            make.bottom.equalTo(card).offset(-16)
-            
-            make.height.equalTo(36)
-        }
-    }
-    
     let scrollView = UIScrollView()
     let contentView = UIView()
     
@@ -156,14 +85,72 @@ class WorkoutLogGeneralSnapKitViewController: UIViewController {
         
         let label = TitleLabel()
         label.text = "Today's Progress"
-        
         card.addSubview(label)
+        
         label.snp.makeConstraints { (make) -> Void in
             make.top.equalTo(card).offset(20)
             make.left.equalTo(card).offset(16)
             make.right.equalTo(card).offset(-16)
             
             make.height.equalTo(24)
+        }
+        
+        let cardButton = CardButton()
+        cardButton.setTitle("Start Workout", for: .normal)
+        card.addSubview(cardButton)
+        
+        cardButton.snp.makeConstraints { (make) -> Void in
+            make.top.equalTo(label.snp.bottom).offset(16)
+            make.left.equalTo(card).offset(16)
+            make.right.equalTo(card).offset(-16)
+            make.bottom.equalTo(card).offset(-16)
+            
+            make.height.equalTo(36)
+        }
+        
+        return card
+    }
+    
+    func createAboutRoutineCard() -> CardView {
+        let card = CardView()
+        self.contentView.addSubview(card)
+        
+        let label = TitleLabel()
+        label.text = "Bodyweight Fitness"
+        card.addSubview(label)
+        
+        label.snp.makeConstraints { (make) -> Void in
+            make.top.equalTo(card).offset(20)
+            make.left.equalTo(card).offset(16)
+            make.right.equalTo(card).offset(-16)
+            
+            make.height.equalTo(24)
+        }
+        
+        let textView = DescriptionTextView()
+        textView.text = "Many people want to improve overall flexibility, but do not know where to begin. This routine should serve as a general jumping-off point for beginners."
+        let sizeThatFits = textView.sizeThatFits(CGSize(width: view.frame.width, height: CGFloat(MAXFLOAT)))
+        card.addSubview(textView)
+        
+        let cardButton = CardButton()
+        cardButton.setTitle("Read More", for: .normal)
+        card.addSubview(cardButton)
+        
+        textView.snp.makeConstraints { (make) -> Void in
+            make.top.equalTo(label.snp.bottom).offset(8)
+            make.left.equalTo(card).offset(12)
+            make.right.equalTo(card).offset(-12)
+            make.bottom.equalTo(cardButton.snp.top).offset(-12)
+            
+            make.height.equalTo(sizeThatFits)
+        }
+        
+        cardButton.snp.makeConstraints { (make) -> Void in
+            make.left.equalTo(card).offset(16)
+            make.right.equalTo(card).offset(-16)
+            make.bottom.equalTo(card).offset(-16)
+            
+            make.height.equalTo(36)
         }
         
         return card
@@ -178,19 +165,17 @@ class WorkoutLogGeneralSnapKitViewController: UIViewController {
         
         let card = self.createTodaysProgressCard()
         card.snp.makeConstraints { (make) -> Void in
-            make.height.equalTo(150)
-            make.left.right.equalTo(contentView).inset(20)
-            make.top.equalTo(contentView).offset(20)
+            make.left.right.equalTo(contentView).inset(16)
+            make.top.equalTo(contentView).offset(16)
         }
         
-        let card2 = CardView()
+        let card2 = self.createAboutRoutineCard()
         contentView.addSubview(card2)
         
         card2.snp.makeConstraints { (make) -> Void in
-            make.height.equalTo(150)
-            make.left.right.equalTo(contentView).inset(20)
-            make.top.equalTo(card.snp.bottom).offset(20)
-            make.bottom.equalTo(contentView).offset(-20)
+            make.left.right.equalTo(contentView).inset(16)
+            make.top.equalTo(card.snp.bottom).offset(16)
+            make.bottom.equalTo(contentView).offset(-16)
         }
     }
 }
